80d0348165bdf4517ec90f1569db557332aafc35
MateuszStolecki <mateusz.stolecki@gmail.com>
2018-04-22 21:34:11 +0200

    Second commit

--- src/java/files/helpers.java
+++ src/java/files/helpers.java
@@ -2,8 +2,15 @@ package files;
 
 import java.util.concurrent.ThreadLocalRandom;
 
+/**
+ * @author Mateusz Stolecki
+ *
+ * Below consist of helping metod used throughout the test
+ *
+ */
 public class helpers {
 
+    /** Method used for conversion of caught string to get just the proper value*/
     public static int convertValueToSimpleInt(String stringValue) {
         String subStr = stringValue.substring(stringValue.lastIndexOf("$")+1);
         String newText = subStr.replace(".00", "").replace(",", "");
@@ -11,12 +18,14 @@ public class helpers {
         return simpleIntValue;
     }
 
+    /** Method used for conversion of caught string to get just the proper value*/
     public static String getMinimumQuantityValue(String str) {
         String subStr = str.substring(str.lastIndexOf("is ")+3);
         String justThevalue = subStr.replace("!", "");
         return  justThevalue;
     }
 
+    /** Method used for generating random string from 1000000 to 9999999*/
     public static String generateRandomString() {
         int randomNum = ThreadLocalRandom.current().nextInt(1000000, 9999999 + 1);
         String randomString = String.valueOf(randomNum);
